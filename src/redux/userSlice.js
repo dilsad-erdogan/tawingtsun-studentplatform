680867c000372af44b2057a8ec31bc7339b0b80d
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-08-12 21:14:48 +0300

    add redux code

---
+++ src/redux/userSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getUserByUID } from "../firebase/auth/users";
+
+export const fetchUserByUID = createAsyncThunk(
+  "user/fetchUserByUID",
+  async (uid) => {
+    const userData = await getUserByUID(uid);
+    return userData;
+  }
+);
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    data: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    clearUser: (state) => {
+      state.data = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUserByUID.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUserByUID.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload;
+      })
+      .addCase(fetchUserByUID.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { clearUser } = userSlice.actions;
+export default userSlice.reducer;
