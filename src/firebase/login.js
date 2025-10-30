2bbe1428f71a0c3bf3caf94d00406afb638d84b4
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-10-30 21:36:41 +0300

    login update code

--- src/firebase/login.js
+++ src/firebase/login.js
@@ -1,15 +1,22 @@
 import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from "./firebase.js";
-
 import toast from 'react-hot-toast';
 
 export const login = async (email, password) => {
-  try{
-    const { user } = await signInWithEmailAndPassword(auth, email, password);
-    return user;
-  } catch (error) {
-    console.error("login error:", error);
-    toast.error("Kullanıcı bilgilerinde hata.");
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (err) {
+    if (err.code === "auth/user-not-found") {
+      toast.error("Kullanıcı bulunamadı");
+    } else if (err.code === "auth/wrong-password") {
+      toast.error("Şifre hatalı");
+    } else if (err.code === "auth/too-many-requests") {
+      toast.error("Çok fazla deneme. Lütfen biraz bekleyin.");
+    } else {
+      toast.error("Bir hata oluştu");
+    }
+    return null;
   }
 };
 
