27aa927649204ed4ebc9f978cb69722688787316
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-11-19 20:02:53 +0300

    login ataması

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Login from "./pages/Login";
 import Trainer from "./pages/Trainer";
 import Admin from "./pages/Admin";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/firebase";
+import { fetchUserByUID, clearUser } from "./redux/userSlice";
 
 function App() {
-  const user = useSelector((state) => state.user.data);
+  const user = useSelector((state) => state.user.data);   // undefined | null | data
+  const dispatch = useDispatch();
+
+  // Firebase kullanıcı kontrolü
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        dispatch(fetchUserByUID(currentUser.uid));  // accounts tablosundan çek
+      } else {
+        dispatch(clearUser());
+      }
+    });
+
+    return () => unsub();
+  }, [dispatch]);
+
+  // Kullanıcı verisi henüz gelmediyse
+  if (user === undefined) {
+    return <div className="flex h-screen items-center justify-center text-xl">Loading...</div>;
+  }
 
   return (
     <div className="h-screen text-black">
       <Router>
         <Routes>
+          {/* --- Login Page --- */}
           <Route path="/login" element={<Login />} />
 
+          {/* --- Trainer Page --- */}
           <Route
             path="/trainer/:authId"
             element={
@@ -23,6 +48,7 @@ function App() {
             }
           />
 
+          {/* --- Admin Page --- */}
           <Route
             path="/admin/:authId"
             element={
@@ -32,10 +58,11 @@ function App() {
             }
           />
 
+          {/* --- Root Page (/) --- */}
           <Route
             path="/"
             element={
-              !user ? (
+              user === null ? (
                 <Navigate to="/login" replace />
               ) : user.isAdmin ? (
                 <Navigate to={`/admin/${user.authId}`} replace />
@@ -45,6 +72,7 @@ function App() {
             }
           />
 
+          {/* --- Unknown route: redirect to root --- */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
