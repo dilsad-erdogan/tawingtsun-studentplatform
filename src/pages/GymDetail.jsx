a310db6f3831b47eea19c892b9ada2d153ec67c1
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2025-11-25 12:41:46 +0300

    error fixed

--- src/pages/GymDetail.jsx
+++ src/pages/GymDetail.jsx
@@ -1,12 +1,19 @@
+import { Toaster } from "react-hot-toast";
 import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const GymDetail = () => {
     const { gymId } = useParams();
 
     return (
-        <div className="p-6">
-            <h1 className="text-2xl font-bold">Salon Detayı</h1>
-            <p>Salon ID: {gymId}</p>
+        <div className="min-h-screen bg-gray-100 gap-5 pt-5">
+            <Toaster position="top-right" />
+
+            <Navbar />
+            <div className="m-2">
+                <h1 className="text-2xl font-bold">Salon Detayı</h1>
+                <p>Salon ID: {gymId}</p>
+            </div>
         </div>
     );
 };
